3747e0d962013049d428ff078885629c1bf9921b
jm1 <jm1441@proximus.be>
2023-06-22 17:52:57 +0200

    Tellen vd artikels per shop is nog niet klaar

--- app/src/main/java/be/hvwebsites/shopping/A4ShopCompetitionList.java
+++ app/src/main/java/be/hvwebsites/shopping/A4ShopCompetitionList.java
@@ -5,22 +5,25 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import be.hvwebsites.libraryandroid4.adapters.NothingSelectedSpinnerAdapter;
-import be.hvwebsites.libraryandroid4.helpers.CheckboxHelper;
-import be.hvwebsites.libraryandroid4.helpers.IDNumber;
+import be.hvwebsites.libraryandroid4.helpers.ListItemHelper;
 import be.hvwebsites.libraryandroid4.repositories.CookieRepository;
 import be.hvwebsites.libraryandroid4.returninfo.ReturnInfo;
-import be.hvwebsites.shopping.adapters.CheckboxListAdapter;
+import be.hvwebsites.shopping.adapters.SmartTextItemListAdapter;
 import be.hvwebsites.shopping.constants.SpecificData;
 import be.hvwebsites.shopping.entities.Product;
+import be.hvwebsites.shopping.entities.ProductInShop;
 import be.hvwebsites.shopping.entities.Shop;
+import be.hvwebsites.shopping.entities.SuperCombination;
+import be.hvwebsites.shopping.helpers.StringIntCombin;
 import be.hvwebsites.shopping.services.FileBaseService;
 import be.hvwebsites.shopping.viewmodels.ShopEntitiesViewModel;
 
@@ -29,11 +32,9 @@ public class A4ShopCompetitionList extends AppCompatActivity {
     private final String deviceModel = Build.MODEL;
     private ShopEntitiesViewModel viewModel;
     private CookieRepository cookieRepository;
-//    private boolean smsOn = false;
 //    private List<Product> productsMatchingShopfilter = new ArrayList<>();
 //    private List<Product> prodinShopMatchingShopFilter = new ArrayList<>();
-    private List<CheckboxHelper> checkboxList = new ArrayList<>();
-    private CheckboxListAdapter cbListAdapter;
+    private List<ListItemHelper> competitionList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,22 +55,164 @@ public class A4ShopCompetitionList extends AppCompatActivity {
                     Toast.LENGTH_LONG).show();
         }
 
+        // Cookies initialiseren
+        cookieRepository = new CookieRepository(fileBaseService.getFileBaseDir());
 
-        // Recyclerview definieren
+        // Recyclerview en adapter definieren
         RecyclerView recyclerView = findViewById(R.id.recyclShopCompetitionList);
-        // ChckbxListAdapter kan vervangen worden door CheckBoxListAdapter !!
-        cbListAdapter = new CheckboxListAdapter(this);
-        recyclerView.setAdapter(cbListAdapter);
+        SmartTextItemListAdapter listAdapter = new SmartTextItemListAdapter(this);
+        recyclerView.setAdapter(listAdapter);
         LinearLayoutManager cbLineairLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(cbLineairLayoutManager);
-        // Indien CheckBoxListAdapter gekozen wordt, dan vlgnd statement geactiveerd worden !!
-        cbListAdapter.setActivityMaster(SpecificData.ACTIVITY_A4SHOPPINGLIST);
+        // Adapter invullen
+        // Activity doorgeven aan adapter
+        listAdapter.setClient(SpecificData.ACTIVITY_A4SHOPCOMPETITION);
+        listAdapter.setReference(SpecificData.LIST_TYPE_1);
+        // Invullen adapter vd recyclerview met competitionlist
+        composeCompetitionList();
+        listAdapter.setReusableList(competitionList);
 
-        // Invullen adapter vd recyclerview met checkboxlist
-        cbListAdapter.setReference(SpecificData.LIST_TYPE_2);
-        cbListAdapter.setCheckboxList(checkboxList);
+        // Titel Activity invullen
         setTitle(SpecificData.TITLE_SHOPPING_LIST);
+
+        // Als er geclicked is op een shop, wordt dat hier verder afgehandeld
+        listAdapter.setOnItemClickListener(new SmartTextItemListAdapter.ClickListener() {
+            @Override
+            public void onItemClicked(int position, View v) {
+                // Shop reconstrueren waarop geclicked is
+                Shop clickedShop = viewModel.getShopByID(competitionList.get(position).getItemID());
+                if (clickedShop == null){
+                    // Shop niet gevonden als shopfilter in cookie steken vr A4Shoppinglist activity
+                    cookieRepository.registerCookie(SpecificData.SHOP_FILTER, String.valueOf(SpecificData.NO_FILTER_INT));
+                }else {
+                    // Shop als shopfilter in cookie steken vr A4Shoppinglist activity
+                    cookieRepository.registerCookie(SpecificData.SHOP_FILTER, String.valueOf(clickedShop.getEntityId().getId()));
+                }
+
+                // Ga naar A4ShoppingList
+                Intent compIntent = new Intent(A4ShopCompetitionList.this, A4ShoppingListActivity.class);
+                startActivity(compIntent);
+            }
+        });
+
+    }
+
+    private void composeCompetitionList3(){
+        List<SuperCombination> prodShopCombins = new ArrayList<>();
+
+        // Alle combinaties prodshops inladen
+        for (int i = 0; i < viewModel.getProductInShopList().size(); i++) {
+            ProductInShop tempProdShop = viewModel.getProductInShopList().get(i);
+            SuperCombination tempCombin = new SuperCombination(
+                    tempProdShop.getFirstID()
+                    ,tempProdShop.getSecondID()
+                    ," "
+                    ," "
+            );
+            prodShopCombins.add(tempCombin);
+        }
+
+        // Combinaties uit produktlist toevoegen
+        for (int i = 0; i < viewModel.getProductList().size(); i++) {
+            if (viewModel.getProductList().get(i).isToBuy()){
+                SuperCombination tempCombin = new SuperCombination(
+                        viewModel.getProductList().get(i).getPreferredShopId()
+                        ,viewModel.getProductList().get(i).getEntityId()
+                        ," "
+                        ," "
+                );
+                prodShopCombins.add(tempCombin);
+            }
+        }
+    }
+
+/*
+    private void composeCompetitionList2(){
+        competitionList.clear();
+
+        // Bepalen per winkel hoeveel artikels deze winkel als prefered hebben
+        for (int i = 0; i < viewModel.getShopList().size(); i++) {
+            Shop tempShop = viewModel.getShopList().get(i);
+            // Bepalen alle checked products met voorkeurwinkel
+            for (int i = 0; i < viewModel.getProductList().size(); i++) {
+                Product tempProduct = viewModel.getProductList().get(i);
+                if ((tempProduct.getPreferredShopId().getId() == tempShop.getEntityId().getId())
+                        && tempProduct.isToBuy()){
+                    StringIntCombin combin = new StringIntCombin();
+                    combin.setTextID(tempShop.getEntityId().getId());
+                    combin.setText(tempShop.getEntityName());
+                    combin.setTeller1((combin.getTeller1()+1));
+                    SuperCombination tempCombinPref = new SuperCombination(
+                            tempProduct.getEntityId()
+                            ,tempProduct.getPreferredShopId()
+                            ," "
+                            ," "
+                    );
+
+                }
+            }
+
+
+        }
+
+    }
+*/
+
+    private void composeCompetitionList(){
+        List<SuperCombination> checkedProducts = new ArrayList<>();
+        List<SuperCombination> productShopCombins = new ArrayList<>();
+        List<StringIntCombin> tempCompetition = new ArrayList<>();
+
+        competitionList.clear();
+
+        // Initialize tempCompetition
+        for (int i = 0; i < viewModel.getShopList().size(); i++) {
+            Shop tempShop = viewModel.getShopList().get(i);
+            StringIntCombin tempCombin = new StringIntCombin();
+            tempCombin.setTextID(tempShop.getEntityId().getId());
+            tempCombin.setText(tempShop.getEntityName());
+            tempCombin.setTeller1(0);
+            tempCombin.setTeller2(0);
+            tempCompetition.add(tempCombin);
+        }
+
+        // Bepalen alle checked products met voorkeurwinkel
+        for (int i = 0; i < viewModel.getProductList().size(); i++) {
+            Product tempProduct = viewModel.getProductList().get(i);
+            if (tempProduct.isToBuy()){
+                // Verhoog teller in tempCompetition
+                for (int j = 0; j < tempCompetition.size(); j++) {
+                    if (tempCompetition.get(i).getTextID() == tempProduct.getPreferredShopId().getId()){
+                        tempCompetition.get(i).setTeller1(tempCompetition.get(i).getTeller1()+1);
+                        // Bewaar combinatie
+                        SuperCombination tempCombinPref = new SuperCombination(
+                                tempProduct.getEntityId()
+                                ,tempProduct.getPreferredShopId()
+                                ," "
+                                ," "
+                        );
+                        checkedProducts.add(tempCombinPref);
+                    }
+                }
+            }
+        }
+
+        // Bepalen alle produktshopcombins
+        for (int i = 0; i < viewModel.getProductInShopList().size(); i++) {
+            ProductInShop tempProdShop = viewModel.getProductInShopList().get(i);
+            if (viewModel.getProductByID(tempProdShop.getSecondID()).isToBuy()){
+                SuperCombination tempCombin = new SuperCombination(
+                        tempProdShop.getSecondID()
+                        ,tempProdShop.getFirstID()
+                        ," "
+                        ," "
+                );
+                productShopCombins.add(tempCombin);
+            }
+        }
 
 
     }
+
+
 }
---
+++ app/src/main/java/be/hvwebsites/shopping/helpers/StringIntCombin.java
@@ -0,0 +1,43 @@
+package be.hvwebsites.shopping.helpers;
+
+public class StringIntCombin {
+    private int textID;
+    private String Text;
+    private int teller1;
+    private int teller2;
+
+    public StringIntCombin() {
+    }
+
+    public int getTextID() {
+        return textID;
+    }
+
+    public void setTextID(int textID) {
+        this.textID = textID;
+    }
+
+    public String getText() {
+        return Text;
+    }
+
+    public void setText(String text) {
+        Text = text;
+    }
+
+    public int getTeller1() {
+        return teller1;
+    }
+
+    public void setTeller1(int teller1) {
+        this.teller1 = teller1;
+    }
+
+    public int getTeller2() {
+        return teller2;
+    }
+
+    public void setTeller2(int teller2) {
+        this.teller2 = teller2;
+    }
+}
